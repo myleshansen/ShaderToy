d104bfa9be59b666786621ef0aaf3d2d651e22f4
Myles Hansen <myleshansen@outlook.com>
2025-11-05 14:32:38 -0800

    Add boilerplate code

---
+++ ShaderToy/ShaderToy.cpp
@@ -0,0 +1,12 @@
+﻿// ShaderToy.cpp : Defines the entry point for the application.
+//
+
+#include "ShaderToy.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
