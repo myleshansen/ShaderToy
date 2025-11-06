cbb8c9143785fe38605c8791b65996d01060714a
Myles Hansen <myleshansen@outlook.com>
2025-11-05 16:48:58 -0800

    Add glfw + glad and initialize glfw window

---
+++ src/ShaderToy.cpp
@@ -0,0 +1,41 @@
+﻿// ShaderToy.cpp : Defines the entry point for the application.
+//
+
+#include "ShaderToy.h"
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+using namespace std;
+
+int main()
+{
+	if (!glfwInit())
+	{
+		return -1;
+	}
+
+	GLFWwindow* window = glfwCreateWindow(640, 480, "ShaderToy", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+	
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		cout << "Failed to initialize GLAD" << endl;
+		return -1;
+	}
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
+	cout << "Hello CMake." << endl;
+	return 0;
+}
